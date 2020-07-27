a9123b15d46d694021ea5097e08ef6d02028d6e9
Phony <1154537887@qq.com>
2020-07-27 23:13:08 +0800

    Add member to rational obj

--- 6_fun_obj/Rational.scala
+++ 6_fun_obj/Rational.scala
@@ -1,3 +1,10 @@
 class Rational(n: Int, d: Int) {
-    println("Created " + n + "/" + d)
+  require(d != 0)
+  val numer: Int = n
+  val denom: Int = d
+
+  override def toString: String = n + "/" + d
+
+  def add(that: Rational): Rational =
+    new Rational(numer * that.denom + denom * that.numer, denom * that.denom)
 }
