3a212fdc705e8c2a22a25d0365118f86920682d3
Phony Lou <louqilin555@gmail.com>
2020-07-31 11:27:01 +0800

    format display

--- 6_fun_obj/Rational.scala
+++ 6_fun_obj/Rational.scala
@@ -1,9 +1,12 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
 
-  override def toString: String = n + "/" + d
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  def this(n: Int) = this(n, 1)
+  override def toString: String = numer + "/" + denom
 
   def add(that: Rational): Rational =
     new Rational(numer * that.denom + denom * that.numer, denom * that.denom)
@@ -11,4 +14,8 @@ class Rational(n: Int, d: Int) {
   def lessThan(that: Rational): Boolean = this.numer * that.denom < this.denom * that.denom
 
   def max(that: Rational): Rational = if (this.lessThan(that)) that else this
+
+  private def gcd(a: Int, b: Int): Int = 
+    if (b == 0) a
+    else gcd(b, a % b)
 }
