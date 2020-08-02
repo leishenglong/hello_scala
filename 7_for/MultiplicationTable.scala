5e4723bb6ba13db54c7ef0f5e1cda7715ab26afc
Phony <1154537887@qq.com>
2020-08-02 22:01:23 +0800

    Multi table

---
+++ 7_for/MultiplicationTable.scala
@@ -0,0 +1,13 @@
+def makeRowSeq(row: Int): Seq[String] =
+  for (col <- 1 to 10) yield {
+    val prod = (col * row).toString
+    val padding = " " * (4 - prod.length)
+    padding + prod
+  }
+
+def makeRow(row: Int): String = makeRowSeq(row).mkString
+
+def multiTable(): String = {
+  val tableSeq = for (row <- 1 to 10) yield makeRow(row)
+  tableSeq.mkString("\n")
+}
