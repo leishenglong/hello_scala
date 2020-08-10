29710b8e038029ec4c54dead69c78c876b51767d
Phony Lou <louqilin555@gmail.com>
2020-08-10 17:54:07 +0800

    Refactor matching logic by using high order function

--- 8_function/FileMatcher.scala
+++ 8_function/FileMatcher.scala
@@ -2,14 +2,18 @@ object FileMatcher {
   private def filesHere = (new java.io.File(".")).listFiles
 
   def filesEnding(query: String) =
-    for (file <- filesHere; if (file.getName.endsWith(query)))
+    for (file <- filesHere; if file.getName.endsWith(query))
       yield file
 
   def filesContain(query: String) =
-    for (file <- filesHere; if (file.getName.contains(query)))
+    for (file <- filesHere; if file.getName.contains(query))
       yield file
 
   def filesMatch(query: String) =
-    for (file <- filesHere; if (file.getName.matches(query)))
+    for (file <- filesHere; if file.getName.matches(query))
+      yield file
+
+  def filesMatching(query: String, matcher: (String, String) => Boolean) =
+    for (file <- filesHere; if matcher(file.getName, query))
       yield file
 }
