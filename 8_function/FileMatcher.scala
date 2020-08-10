2cde3a83247d81336b6e9e35d581210e7014c685
Phony Lou <louqilin555@gmail.com>
2020-08-10 17:58:47 +0800

    Update implementatio

--- 8_function/FileMatcher.scala
+++ 8_function/FileMatcher.scala
@@ -2,16 +2,13 @@ object FileMatcher {
   private def filesHere = (new java.io.File(".")).listFiles
 
   def filesEnding(query: String) =
-    for (file <- filesHere; if file.getName.endsWith(query))
-      yield file
+    filesMatching(query, _.endsWith(_))
 
   def filesContain(query: String) =
-    for (file <- filesHere; if file.getName.contains(query))
-      yield file
+    filesMatching(query, _.contains(_))
 
   def filesMatch(query: String) =
-    for (file <- filesHere; if file.getName.matches(query))
-      yield file
+    filesMatching(query, _.matches(_))
 
   def filesMatching(query: String, matcher: (String, String) => Boolean) =
     for (file <- filesHere; if matcher(file.getName, query))
