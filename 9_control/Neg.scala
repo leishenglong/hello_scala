80d5902ba12176aaa580c1d4b79d330e3a94170d
Phony Lou <louqilin555@gmail.com>
2020-08-17 14:44:33 +0800

    High order function to avoid of using for loop

--- 9_control/Neg.scala
+++ 9_control/Neg.scala
@@ -1,15 +1,15 @@
-object Neg {
-  def containsNeg(nums: List[Int]): Boolean = {
-    var exists = false
-    for (num <- nums if num < 0)
-      exists = true
+/**
+ * Run with
+ * $ scalac Neg.scala
+ * $ scala Neg
+ */
 
-    exists
-  }
+object Neg {
+  def containsNeg(nums: List[Int]): Boolean = nums.exists(_ < 0)
 
   def main(args: Array[String]): Unit = {
-    val a = List(1,2,4)
-    val b = List(1,2,-4,6)
+    val a = List(1, 2, 4)
+    val b = List(1, 2, -4, 6)
     println(containsNeg(a))
     println(containsNeg(b))
   }
