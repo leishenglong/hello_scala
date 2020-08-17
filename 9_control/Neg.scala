98cb701e6bcbfcfd25b8910328d1752ed547c8cf
Phony Lou <louqilin555@gmail.com>
2020-08-17 14:52:03 +0800

    Add contain odd function

--- 9_control/Neg.scala
+++ 9_control/Neg.scala
@@ -6,11 +6,18 @@
 
 object Neg {
   def containsNeg(nums: List[Int]): Boolean = nums.exists(_ < 0)
+  def containsOdd(nums: List[Int]): Boolean = nums.exists(_ % 2 == 1)
 
   def main(args: Array[String]): Unit = {
     val a = List(1, 2, 4)
     val b = List(1, 2, -4, 6)
     println(containsNeg(a))
     println(containsNeg(b))
+
+    val numsWithoutOdd = List(2,4,8)
+    val numsWithOdd = List(2,1,8)
+    println(containsOdd(numsWithoutOdd))
+    println(containsOdd(numsWithOdd))
+
   }
 }
