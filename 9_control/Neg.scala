e138c2d2898de55dfb7609dc49cf201e4a6c5df8
Phony Lou <louqilin555@gmail.com>
2020-08-17 14:36:56 +0800

    Using loop to find negative value

---
+++ 9_control/Neg.scala
@@ -0,0 +1,17 @@
+object Neg {
+  def containsNeg(nums: List[Int]): Boolean = {
+    var exists = false
+    for (num <- nums)
+      if (num < 0)
+        exists = true
+
+    exists
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a = List(1,2,4)
+    val b = List(1,2,-4,6)
+    println(containsNeg(a))
+    println(containsNeg(b))
+  }
+}
