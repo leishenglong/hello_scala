c18308511547e04af269f5a700c6b2af6c6fc41a
Phony <1154537887@qq.com>
2020-08-17 22:25:23 +0800

    Add supper constractor

--- 10_combine/Element.scala
+++ 10_combine/Element.scala
@@ -5,3 +5,8 @@ abstract class Element {
 }
 
 class ArrayElement(val contents: Array[String]) extends Element
+
+class LineElement(s: String) extends ArrayElement(Array(s)) {
+  override def height: Int = 1
+  override def width: Int = s.length
+}
