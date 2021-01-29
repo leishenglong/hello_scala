2b63bf6beb020e725e902e07536b773f7350fccc
Phony Lou <louqilin555@gmail.com>
2021-01-29 16:59:45 +0800

    try tail recursive

---
+++ src/test/scala/com/training/TailRecursiveSpec.scala
@@ -0,0 +1,24 @@
+package com.training
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.annotation.tailrec
+
+class TailRecursiveSpec extends AnyWordSpec with Matchers {
+
+  "Tail Recursive Spec" should {
+
+    "build a tail recursive" in {
+      def bang(x: Int): Int = {
+        if (x == 0) throw new Exception("bang !!!")
+        else bang(x - 1)
+      }
+
+      the[Exception] thrownBy {
+        bang(5)
+      }
+    }
+  }
+
+}
