0e361f500cc04bbf06bbcbaf40c89a34f56db4f7
Phony Lou <louqilin555@gmail.com>
2021-02-02 14:40:39 +0800

    show how to use curring

--- src/test/scala/com/training/CurrySpec.scala
+++ src/test/scala/com/training/CurrySpec.scala
@@ -23,6 +23,21 @@ class CurrySpec extends AnyWordSpec with Matchers {
       val plusOne = curriedSum(1)_
       plusOne(2) shouldEqual 3
     }
+
+    "show curry function" in {
+      def filter(xs: List[Int], p: Int => Boolean): List[Int] = {
+        if (xs.isEmpty) xs
+        else if (p(xs.head)) xs.head :: filter(xs.tail, p)
+        else filter(xs.tail, p)
+      }
+
+      def modN(n: Int)(x: Int) = (x % n) == 0
+
+      val nums = List.range(1, 9)
+
+      filter(nums, modN(2)) shouldEqual List(2, 4, 6, 8)
+      filter(nums, modN(3)) shouldEqual List(3, 6)
+    }
   }
 
 }
