8742accd3f636698ecb4638aaccc9a69fdd2442f
Phony Lou <louqilin555@gmail.com>
2021-02-03 18:37:10 +0800

    Palindrome spec. Question: How can I make a words list more than 1000000?

---
+++ src/test/scala/com/training/PalindromeSpec.scala
@@ -0,0 +1,31 @@
+package com.training
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.reflect.internal.util.StringOps.words
+
+
+class PalindromeSpec extends AnyWordSpec with Matchers {
+
+  "Palindrome Spec" should {
+
+    "do not use lazy" in {
+      def isPalindrome(x: String) = x == x.reverse
+      def findFirstPalindrome(s: Seq[String]) = s find isPalindrome
+
+      val words = List("This", "is", "anna", "able", "to", "get", "cake")
+      findFirstPalindrome(words take 4) shouldEqual Some("anna")
+    }
+
+    "use lazy" in {
+      def isPalindrome(x: String) = x == x.reverse
+      def findFirstPalindrome(s: Seq[String]) = s find isPalindrome
+
+      val words = List("This", "is", "anna", "able", "to", "get", "cake")
+      findFirstPalindrome(words.view take 4) shouldEqual Some("anna")
+    }
+
+  }
+
+}
