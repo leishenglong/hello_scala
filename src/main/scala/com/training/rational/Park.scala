4483f2ab6842340a5b0cf50b00f0841278e28391
leishenglong <18710527976@163.com>
2021-08-04 10:11:30 +0800

    change parkinglot to park

--- src/main/scala/com/training/rational/Park.scala
+++ src/main/scala/com/training/rational/Park.scala
@@ -2,15 +2,14 @@ package com.training.rational
 
 import scala.:+
 
-case class ParkPlot(
-                    var cars: Array[Car]  = Array.empty) {
-  def this(size : Int){
+case class Park(var cars: Array[Car] = Array.empty) {
+  def this(size: Int) {
     this(new Array[Car](size))
   }
-  def parkingCar(car: Car): Int = {
 
-    for ( i <- 0 until cars.length) {
-      if(cars(i)==null){
+  def parkingCar(car: Car): Int = {
+    for (i <- 0 until cars.length) {
+      if (cars(i) == null) {
         cars(i) = car
         return i
       }
@@ -19,8 +18,8 @@ case class ParkPlot(
   }
 
   def movingCar(position: Int): Car = {
-    for ( i <- 0 until cars.length) {
-      if(i==position){
+    for (i <- 0 until cars.length) {
+      if (i == position) {
         val car = cars(i)
         cars(i) = null
         return car
--- src/test/scala/com/training/ParkingLotSpec.scala
+++ src/test/scala/com/training/ParkingLotSpec.scala
@@ -1,6 +1,6 @@
 package com.training
 
-import com.training.rational.{Car, ParkPlot}
+import com.training.rational.{Car, Park}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -8,15 +8,9 @@ class ParkingLotSpec extends AnyWordSpec with Matchers {
 
   "ParkingLot Spec" should {
 
-    "do something" in {
-      val args = Array("a", "b", "c")
-      val result = args.drop(1)
-      println(result.length)
-    }
-
     "given car and plot when parking Car then return position" in {
       val car = Car("111")
-      val parkPlot = ParkPlot(new Array[Car](2))
+      val parkPlot = Park(new Array[Car](2))
       val position = parkPlot.parkingCar(car);
       position shouldEqual 0
       val p = parkPlot.parkingCar(Car("112"))
@@ -24,12 +18,12 @@ class ParkingLotSpec extends AnyWordSpec with Matchers {
     }
 
     "given car and plot when moving car then return car" in {
-      val parkPlot = ParkPlot( Array(Car("123")))
+      val parkPlot = Park( Array(Car("123")))
       parkPlot.movingCar(0) shouldEqual Car("123")
     }
 
     "given two cars and plot when moving one by one then return cars by orderly" in {
-      val parkPlot = ParkPlot( Array(Car("123"), Car("456")))
+      val parkPlot = Park( Array(Car("123"), Car("456")))
 
       val firstCar = parkPlot.movingCar(0)
       firstCar shouldEqual Car("123")
