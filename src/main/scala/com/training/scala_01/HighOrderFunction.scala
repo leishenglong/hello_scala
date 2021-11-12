0c8c7c51ecca69dcda6d7196acff913cef1881df
leishenglong <18710527976@163.com>
2021-11-12 13:44:13 +0800

    Higher function

--- src/main/scala/com/training/scala_01/HighOrderFunction.scala
+++ src/main/scala/com/training/scala_01/HighOrderFunction.scala
@@ -8,11 +8,15 @@ object HighOrderFunction {
     else callNTime(f, n - 1, f(x))
   }
 
-  def callNTimeAnother(f: Int => Int, n: Int): Int => Int = {
+  def callNTimeBetter(f: Int => Int, n: Int): Int => Int = {
     if (n <= 0) (x: Int) => x
-    else (x: Int) => callNTimeAnother(f, n - 1)(f(x))
+    else (x: Int) => callNTimeBetter(f, n - 1)(f(x))//callNTimeBetter return f apply f(x)
   }
 
+  val plusOne=(x:Int)=>x+1
+
+  val plus10 = callNTimeBetter(plusOne,10)
+  println(plus10(1))
   val superAdd: Int => (Int => Int) = (a: Int) => (b: Int) => a + b
   //  val superAdd:Int=>Int=>Int=(a:Int)=>(b:Int)=>a+b
 
@@ -28,15 +32,14 @@ object HighOrderFunction {
   def fibonacciSequence(i: Int, map: Map[Int, Int]): String = {
     if (i == 1) return "0"
     if (i == 2) return "0 1"
+    var str = "0 1"
     if (i > 2) {
-      var str = "0 1"
       for (x <- 3 to i) {
         map += (x->(map(x-1)+map(x-2)))
         str=str.concat(" "+(map(x-1)+map(x-2)))
       }
-      str
     }
-    else ""
+    str
   }
 
 }
