8824042adc58dba49804ea97a3ee91a5c971a2c4
leishenglong <18710527976@163.com>
2021-11-15 11:18:03 +0800

    word count

--- src/main/scala/com/training/WordCount.scala
+++ src/main/scala/com/training/WordCount.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 //https://github.com/fxsjy/jieba
 object WordCount {
 
-  val reg="\\u3002|\\uff1f|\\uff01|\\uff0c|\\u3001|\\uff1b|\\uff1a|\\u201c|\\u201d|\\u2018|\\u2019|\\uff08|\\uff09|\\u300a|\\u300b|\\u3008|\\u3009|\\u3010|\\u3011|\\u300e|\\u300f|\\u300c|\\u300d|\\ufe43|\\ufe44|\\u3014|\\u3015|\\u2026|\\u2014|\\uff5e|\\ufe4f|\\uffe5"
+  val reg = "\\u3002|\\uff1f|\\uff01|\\uff0c|\\u3001|\\uff1b|\\uff1a|\\u201c|\\u201d|\\u2018|\\u2019|\\uff08|\\uff09|\\u300a|\\u300b|\\u3008|\\u3009|\\u3010|\\u3011|\\u300e|\\u300f|\\u300c|\\u300d|\\ufe43|\\ufe44|\\u3014|\\u3015|\\u2026|\\u2014|\\uff5e|\\ufe4f|\\uffe5"
 
 
   def main(args: Array[String]): Unit = {
@@ -16,25 +16,20 @@ object WordCount {
     val linesList = Source.fromFile(path).getLines().toList
 
     val jieba = new JiebaSegmenter
-    val wordNum=linesList.flatMap(l => l.split(reg)).flatMap(l => jieba.sentenceProcess(l).asScala).
-//    val wordNum=linesList.flatMap(l => jieba.sentenceProcess(l).asScala).
-      map((_,1)).groupBy(_._1).map(s=>(s._1,s._2.size)).toList.sortBy(_._2).reverse.slice(0,20)
-//    println(wordNum)
-    val wordTuples=processLines(linesList,jieba)
-    println(wordTuples)
+    val wordTuples = processLines(linesList, jieba)
+    println(countWord(wordTuples).toList.sortBy(_._2).reverse.slice(0,20))
 
   }
+
   def countWord(tuples: List[(String, Int)]) = for {
 
-        map<-tuples.groupBy(_._1).map(s=>(s._1,s._2.size)).toList.sortBy(_._2).reverse.slice(0,20)
+    (word, listCount) <- tuples.groupBy(_._1)
+    wordCount = (word, listCount.size)
+  } yield wordCount
 
-  } yield map
-
-  def processLines(strings:List[String],jiebaSegmenter: JiebaSegmenter)=for {
-    line<-strings
-    setence<-line.split(reg)
-    (word,number)<-jiebaSegmenter.sentenceProcess(setence).asScala.map((_,1))
-
-
-  }yield (word,number)
+  def processLines(strings: List[String], jiebaSegmenter: JiebaSegmenter) = for {
+    line <- strings
+    setence <- line.split(reg)
+    (word, number) <- jiebaSegmenter.sentenceProcess(setence).asScala.map((_, 1))
+  } yield (word, number)
 }
